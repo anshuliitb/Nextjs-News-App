6d999de86bbc912d505ec399255d048946ccc4de
Anshul Mahor <anshulmahoriitb@gmail.com>
2025-12-27 21:44:37 +0530

    navlink fixed

--- components/nav-link.js
+++ components/nav-link.js
@@ -5,9 +5,21 @@ import { usePathname } from 'next/navigation';
 
 export default function NavLink({ href, children }) {
   const path = usePathname();
+  console.log(path);
+  
 
+  function isActiveLink() {
+        if (path=="/news/create" && children === "News") {
+          return "";
+        }else if (path.startsWith(href)) {
+          return "active";
+        }
+        else {
+          return "";
+        } 
+      }
   return (
-    <Link href={href} className={ path !=="/news/create" && path.startsWith(href)  ? 'active' : undefined}>
+    <Link href={href} className={isActiveLink()}>
       {children}
     </Link>
   );
